5ec43a36ceeb9100037d586ff1c5fe25e63d8ae2
Sarah Burnside <sarahburnside787@gmail.com>
2024-05-01 11:13:51 -0400

    settings fix

--- blog_api_backend/blogapi/settings.py
+++ blog_api_backend/blogapi/settings.py
@@ -60,13 +60,13 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "backend.User"
 
 MIDDLEWARE = [
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "blogapi.urls"
